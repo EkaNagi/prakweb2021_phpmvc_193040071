abea7935b98d09d3a8cb66459d8358ca9b3450d1
Tresna Eka Widiana <ideal.nagi@gmail.com>
2021-12-24 08:31:45 +0700

    Upload semua tugas mvc php

---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+<div class="bg-light mt-4 p-5 rounded-lg m-3 ">
+  <h1 class="display-4">Selamat Datang di Website Saya</h1>
+  <p class="lead">Halo, nama saya <?=$data['nama']; ?> </p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, nobis.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+</div>
